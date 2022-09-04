bd349e8bc06b84676ebc626cb138a80be85fc63c
afaruqii <afaruqii@users.noreply.github.com>
2022-09-04 02:29:25 +0000

    deploy: ed0e84574cce9e34a4967d6c1f632155a5489a23

---
+++ precache-manifest.9191467d537d760dcbc12ab3439317a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bd4ad8a93327cd00a9b",
+    "url": "./static/css/main.15c60d04.chunk.css"
+  },
+  {
+    "revision": "8bd4ad8a93327cd00a9b",
+    "url": "./static/js/main.8bd4ad8a.chunk.js"
+  },
+  {
+    "revision": "8fda2745e87c325a21cc",
+    "url": "./static/js/1.8fda2745.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a1a9894b2045f4bb44d2e62ebbae3238",
+    "url": "./index.html"
+  }
+];
